81f87d614cfef0d6740aa5cf2253b43e1dbebe97
aleja <aleja@172.19.141.159>
2024-12-11 12:26:38 +0100

    ñ

---
+++ MaximoComunDivisor/src/MaximoComunDivisor.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class MaximoComunDivisor {
+	static double n;
+	static int m;
+	public static void main(String[] args) {
+		entrada();
+		double maximocomundivisior = calculo();
+		System.out.println("El MCD es: " + maximocomundivisior);
+	}
+	public static void entrada() {
+		
+		Scanner sc = new Scanner (System.in);
+		System.out.println("Introduzca 2 numeros n y m tal que n<m");
+		System.out.println("Introduzca numero n");
+		n = sc.nextInt();
+		System.out.println("Introduzca numero m");
+		m = sc.nextInt();
+		sc.close();
+		if (n>m) {
+			System.out.print("Numeros no validos");
+			System.exit(0);;
+		}
+
+	}
+    public static int calculo() {
+    	double resto = m%n;
+    	if (resto == 0) {
+    		return (int) n;
+    	}
+    	else {
+    		m = (int) n;
+    		n = (resto);
+    		calculo();
+    	}
+    	return (int) n;
+    }
+}
